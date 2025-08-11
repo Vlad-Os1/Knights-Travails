29d74f5959c353945e19484ce1f28c516be0ac87
Vlad_Os <oshovskiyvlad28@gmail.com>
2025-08-11 19:24:28 +0300

    feat: implement search algorithm for Knight Moves

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ class ChessBoard {
       [-1, -2],
       [-2, -1],
     ];
-    this.size = 8; //our chessboard is 8x8
+    this.size = 8;
   }
   inBounds(x, y) {
     return x > 0 && x < this.size && y > 0 && y < this.size;
@@ -30,41 +30,40 @@ class ChessBoard {
         neighbors.push([newX, newY]);
       }
     });
-    console.log(neighbors);
+    return neighbors;
   }
   knightMoves(start, end) {
-    // first is start, second is end
-    // ( [3, 4] [2, 2]) => returns in  1 move and the path is [3, 4], [2, 2]
     let [x, y] = start;
     let [x2, y2] = end;
-    let queue = [[x, y]];
-    const visited = new Set([start.toString()]);
-    console.log(visited);
-    // while (queue.length > 0) {}
-    console.log(start);
-    console.log(end);
+    let queue = [[[x, y]]];
+
+    const visited = Array.from({ length: this.size }, () =>
+      Array(this.size).fill(false)
+    );
+    visited[x][y] = true;
+    while (queue.length > 0) {
+      let currentPath = queue.shift();
+      let current = currentPath[currentPath.length - 1];
+
+      let currentNeighbors = this.neighbors(current);
+      for (const neighbor of currentNeighbors) {
+        let [dx, dy] = neighbor;
+        if (dx == x2 && dy == y2) {
+          return console.log(
+            `You made it in ${currentPath.length} ${
+              currentPath.length === 1 ? 'move' : 'moves'
+            }. Here's your path: `,
+            currentPath.concat([neighbor])
+          );
+        }
+        if (!visited[dx][dy]) {
+          visited[dx][dy] = true;
+          queue.push([...currentPath, neighbor]);
+        }
+      }
+    }
   }
 }
-//  BFS Method i used in BST
-//  levelOrder(callback) {
-//     if (!this.root) return [];
-//     let queue = [this.root];
-//     while (queue.length > 0) {
-//       const current = queue.shift();
-//       if (callback) {
-//         callback(current);
-//       } else {
-//         throw new Error('Callback is required');
-//       }
-//       if (current.left) {
-//         queue.push(current.left);
-//       }
-//       if (current.right) {
-//         queue.push(current.right);
-//       }
-//     }
-//     return;
-//   }
+
 let board = new ChessBoard();
-board.knightMoves([3, 4], [2, 2]);
-board.neighbors([3, 4]);
+board.knightMoves([3, 3], [4, 3]);
