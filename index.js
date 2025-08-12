c27fe0b35a549c5cee0878ade42bd807a60d1f43
Vlad_Os <oshovskiyvlad28@gmail.com>
2025-08-13 01:03:50 +0300

    docs: add comments

--- index.js
+++ index.js
@@ -14,12 +14,14 @@ class ChessBoard {
     this.size = 8;
   }
   inBounds(x, y) {
+    // Check if the position is inside board boundaries
     return x >= 0 && x < this.size && y >= 0 && y < this.size;
   }
   neighbors(position) {
-    let [x, y] = position;
+    let [x, y] = position; //get x and y coordinate of a position
     let neighbors = [];
     this.moves.forEach((move) => {
+      // run through all moves and add them in neighbors array if they are inBounds
       let dx = move[0];
       let dy = move[1];
 
@@ -39,9 +41,10 @@ class ChessBoard {
 
     const visited = Array.from({ length: this.size }, () =>
       Array(this.size).fill(false)
-    );
+    ); // create a 2D array to represent moves that were already made
     visited[x][y] = true;
     while (queue.length > 0) {
+      // get the path and the cell
       let currentPath = queue.shift();
       let current = currentPath[currentPath.length - 1];
 
@@ -58,7 +61,8 @@ class ChessBoard {
         }
         if (!visited[dx][dy]) {
           visited[dx][dy] = true;
-          queue.push([...currentPath, neighbor]);
+          queue.push([...currentPath, neighbor]); // add the new updated path with the neighbor cell
+          console.log(queue); // log queue for a better visualization
         }
       }
     }
